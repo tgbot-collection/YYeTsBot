1a700ce18f053479738995f6bf706940e508dfef
BennyThink <benny.think@gmail.com>
2021-01-19 23:41:29 +0800

    base test case with setup and teardown

--- tests/test_fansub.py
+++ tests/test_fansub.py
@@ -10,22 +10,21 @@ from fansub import BaseFansub, YYeTs
 
 
 class TestBaseFunsub(unittest.TestCase):
-    def setUp(self) -> None:
-        self.ins = BaseFansub()
-        self.cookie_jar = dict(name="hello")
-        self.ins.cookie_file = "test_cookies.dump"  # generate on tests/test_cookies.dump
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.ins = BaseFansub()
+        cls.cookie_jar = dict(name="hello")
+        cls.ins.cookie_file = "test_cookies.dump"  # generate on tests/test_cookies.dump
 
-    def tearDown(self) -> None:
-        self.ins.redis.flushall()
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls().ins.redis.flushall()
+        os.unlink(cls().ins.cookie_file)
 
     def test_save_cookies(self):
         self.ins.__save_cookies__(self.cookie_jar)
         exists = os.path.exists(self.ins.cookie_file)
         self.assertTrue(exists)
-
-    def test_load_cookies(self):
-        cookie = self.ins.__load_cookies__()
-        self.assertEqual(cookie, self.cookie_jar)
 
     def test_get_from_cache(self):
         value = self.ins.__get_from_cache__("http://test.url", "__hash__")
@@ -37,6 +36,10 @@ class TestBaseFunsub(unittest.TestCase):
         self.ins.__save_to_cache__(url, self.cookie_jar)
         cache_copy = self.ins.__get_from_cache__(url, "never mind method")
         self.assertEqual(cache_copy, self.cookie_jar)
+
+
+class YYeTsTest(unittest.TestCase):
+    pass
 
 
 if __name__ == '__main__':
