e6333a4477b2f012bf62cfde94ab430234f9a208
BennyThink <benny.think@gmail.com>
2021-06-12 20:38:32 +0800

    captcha valid once, svg as static, add filezie in db_dump

--- yyetsweb/server.py
+++ yyetsweb/server.py
@@ -510,6 +510,7 @@ class CaptchaHandler(BaseHandler):
         if not correct_code:
             return {"status": False, "message": "验证码已过期"}
         if user_input.lower() == correct_code.lower():
+            cls.redis.r.delete(correct_code)
             return {"status": True, "message": "验证通过"}
         else:
             return {"status": False, "message": "验证码错误"}
@@ -629,14 +630,23 @@ class NotFoundHandler(BaseHandler):
 
 
 class HelpHandler(BaseHandler):
+    @staticmethod
+    def sizeof_fmt(num: int, suffix='B'):
+        for unit in ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi']:
+            if abs(num) < 1024.0:
+                return "%3.1f%s%s" % (num, unit, suffix)
+            num /= 1024.0
+        return "%.1f%s%s" % (num, 'Yi', suffix)
+
     def file_info(self, file_path) -> dict:
         result = {}
         if iter(file_path):
             for fp in file_path:
                 try:
                     checksum = self.checksum(fp)
-                    date = self.ts_date(os.stat(fp).st_ctime)
-                    result[fp] = [checksum, date]
+                    creation = self.ts_date(os.stat(fp).st_ctime)
+                    size = self.sizeof_fmt(os.stat(fp).st_size)
+                    result[fp] = [checksum, creation, size]
                 except Exception as e:
                     result[fp] = str(e), ""
         return result
@@ -676,7 +686,8 @@ class DBDumpHandler(HelpHandler):
             filename = os.path.basename(file)
             result[filename] = {
                 "checksum": value[0],
-                "date": value[1]
+                "date": value[1],
+                "size": value[2],
             }
 
         return result
@@ -706,7 +717,8 @@ class RunServer:
         (r'/help.html', HelpHandler),
         (r'/api/db_dump', DBDumpHandler),
         (r'/', IndexHandler),
-        (r'/(.*\.html|.*\.js|.*\.css|.*\.png|.*\.jpg|.*\.ico|.*\.gif|.*\.woff2|.*\.gz|.*\.zip)', web.StaticFileHandler,
+        (r'/(.*\.html|.*\.js|.*\.css|.*\.png|.*\.jpg|.*\.ico|.*\.gif|.*\.woff2|.*\.gz|.*\.zip|.*\.zip)',
+         web.StaticFileHandler,
          {'path': static_path}),
     ]
     settings = {
