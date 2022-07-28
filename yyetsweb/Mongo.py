8800d8e33033f3a3cb5e9fe9f05e40f211705cfd
BennyThink <benny.think@gmail.com>
2022-07-28 22:33:00 +0800

    fix code scanning alerts

--- yyetsweb/Mongo.py
+++ yyetsweb/Mongo.py
@@ -838,7 +838,7 @@ class DoubanMongoResource(DoubanResource, Mongo):
         douban_item = soup.find_all("div", class_="content")
 
         fwd_link = unquote(douban_item[0].a["href"])
-        douban_id = re.findall(r"https://movie.douban.com/subject/(\d*)/&query=", fwd_link)[0]
+        douban_id = re.findall(r"https://movie\.douban\.com/subject/(\d*)/.*", fwd_link)[0]
         final_data = self.get_craw_data(cname, douban_id, resource_id, search_html, session)
         douban_col.insert_one(final_data.copy())
         final_data.pop("raw")
